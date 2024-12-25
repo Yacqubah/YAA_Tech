bb45065735209c2e22f141d29fc1f3e2aebef389
Abdiwahid Bishar Hajir <abdiwahidhajir91@gmail.com>
2024-12-25 12:40:19 -0800

    Python backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,59 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fruits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/fruits')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ fruits: data });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  render() {
+    const { fruits } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div>
+            <h2>Fruits List</h2>
+            <ul>
+             {console.log(this.state.fruits)}
+            </ul>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
